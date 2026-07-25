06b1091c8dafdd80b35f8146fd01fa082d39e1dd
Rhythm77-step <tripathirhythm247@gmail.com>
2026-07-25 15:11:54 +0530

    1st commit

---
+++ lockingsystem.py
@@ -0,0 +1,9 @@
+def lock_system():
+    data={ }
+    num=int(input("Enter how many names you want to add in system= "))
+    for i in range(1,num+1)
+    name=input(f"Enter name{i} = ")
+    password=(input(f"Enter {names} password= "))
+    
+
+
