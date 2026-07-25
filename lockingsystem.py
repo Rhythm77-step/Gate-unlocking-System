62eaffb7738131881854ab320f55fcd880105034
Rhythm77-step <tripathirhythm247@gmail.com>
2026-07-25 16:04:42 +0530

    update code

--- lockingsystem.py
+++ lockingsystem.py
@@ -1,9 +1,16 @@
 def lock_system():
-    data={ }
+    user_data={ }
     num=int(input("Enter how many names you want to add in system= "))
     for i in range(1,num+1)
     name=input(f"Enter name{i} = ")
-    password=(input(f"Enter {names} password= "))
+    password=(input(f"Enter {name} password= "))
+    user_data[name]=password
+    print(user_data)
+    
+
+
+
+
     
 
 
