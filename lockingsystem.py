e83ca0785a1a31853f4271c524e714e6c348105a
Rhythm77-step <tripathirhythm247@gmail.com>
2026-07-25 10:54:41 +0000

    Fixing error

--- lockingsystem.py
+++ lockingsystem.py
@@ -1,13 +1,13 @@
 def lock_system():
     user_data={ }
-    num=int(input("Enter how many names you want to add in system= "))
-    for i in range(1,num+1)
-    name=input(f"Enter name{i} = ")
-    password=(input(f"Enter {name} password= "))
-    user_data[name]=password
+    num=int(input("Enter how many persons you want to add in system= "))
+    for i in range(1,(num+1)):
+        name=input(f"Enter name{i} = ")
+        password=(input(f"Enter {name} password= "))
+        user_data[name]=password
     print(user_data)
     
-
+lock_system()
 
 
 
