c457be07c40a1933698d0d8c32fbaac3cef33bcd
Rhythm77-step <tripathirhythm247@gmail.com>
2026-07-28 14:53:40 +0000

    add change

--- lockingsystem.py
+++ lockingsystem.py
@@ -3,17 +3,13 @@ def lock_system():
     user_data={ }
     num=int(input("Enter how many persons you want to add in system= "))
     for i in range(1,(num+1)):
-        i=1
-        name=input(f"Enter name{i} = ")
-        name_list=[]
-        name_list.append(name)                                                      
+        i=1 
+        name=input(f"Enter name{i} = ")                                                      
         password=(input(f"Enter {name} password= "))
-        pass_list=[]
-        pass_list.append(password)
         user_data[name]=password
         user_name=input("Enter your name= ")
         i+=1
-    if user_name in name_list:
+    if user_name in 
         user_pass=input("Enter your password= ")
     else:
         print("Invalid User name")
