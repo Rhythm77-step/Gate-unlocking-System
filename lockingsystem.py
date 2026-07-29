3814ceb62266641872bb7b93fad59ec2bc0a7ea6
Rhythm77-step <tripathirhythm247@gmail.com>
2026-07-29 11:55:38 +0530

    add new function

--- lockingsystem.py
+++ lockingsystem.py
@@ -1,14 +1,16 @@
-def lock_system():
+def user_data():
     #making empty dictionary
-    user_data={ }
+    data={ }
     num=int(input("Enter how many persons you want to add in system= "))
     for i in range(1,(num+1)):
         i=1 
         name=input(f"Enter name{i} = ")                                                      
         password=(input(f"Enter {name} password= "))
-        user_data[name]=password
+        data[name]=password
         i+=1
-    name_list=list(user_data.keys())    
+"""        
+def lock_system():        
+    name_list=list(data.keys())    
     user_name=input("Enter your name= ")
     if 
 
@@ -16,12 +18,12 @@ def lock_system():
         user_pass=input("Enter your password= ")
     else:
         print("Invalid User name")
-    if user_data[user_name]]==user_pass:
+    if data[user_name]==user_pass:
       print("---Gate Unlocked---")
     else:
         print("Wrong Password")
 lock_system()
-
+"""
             
 
 
